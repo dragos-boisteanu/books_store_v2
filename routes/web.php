20e27035ee8afb1811c36e825963390916f1c882
dragos720 <159753852456Gh>
2021-01-17 14:42:33 +0200

    changed route name from singular to plurar

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/search', 'Web\Client\SearchController@index')->name('search');
 
 Route::get('books/{id}', 'Web\Client\BookController@show')->name('books.show');
 
-Route::get('/category/{id}', 'Web\CategoryController')->name('category-books.show');
+Route::get('/categories/{id}', 'Web\CategoryController')->name('category-books.show');
 Route::get('/authors/{id}', 'Web\AuthorsController')->name('authors-books.show');
 Route::get('/publishers/{id}', 'Web\PublisherController')->name('publishers-books.show');
 
