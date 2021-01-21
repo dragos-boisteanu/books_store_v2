f65fc841a18ba4ad956dc811ce80e25219c96c81
Dragos <dragos360@yahoo.com>
2021-01-21 13:44:38 +0200

    updated title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- <title>{{ config('app.name', 'Books store') }}</title> --}}
+    <title>Books store</title>
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
